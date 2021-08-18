21784849855acf44cc96059830d14d8d17139430
Devin Mullin <devin.mullin90@gmail.com>
2021-08-18 13:53:53 -0500

    completed!

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const main = document.getElementById("main")
+Element.classList.remove("main")
+
+const newHeader = document.createElement("h1")
+
+document.body.append(newHeader)
+
+newHeader.classList.add("victory")
+
+newHeader.textContent = "DEVIN is the champion"
